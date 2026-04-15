86f46a32a752369eb9c2a9c6390ea4feb92749eb
nayaraperez <nayaraperez@liceovvh.cl>
2026-04-15 09:27:39 -0400

    okokok

---
+++ 01_Introduccion/1_Hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+//prueba de conexion con js
+console.log("Conexion exitosa con js externo...");
+
+// 1)Hola mundo con variable y const.
+const saludo="Hola a todos,";
+let destinatario="estudiantes";
+//inprecion de saludo concatenando con (+)
+console.log(saludo+"para los " + destinatario);
+
+//2) Saludar a una persona ingresada por el teclado.
+let nombre= prompt("ingrese su nombre");
+//inprimir un nombre ingresado con un prompt
+console.log("Hola, " + nombre);
---
+++ 02_Tipos_de_datos/1._Tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("Conexion con js correcta...")
+/*
+Dato: Tipo texto(string)
+concatenacion de texto con el signo +
+podemosunir texto y variables
+*/
+const nombre ="Nayara";
+const apellido ="Pérez";
+//Unimos ambas contantes con un texto extra.
+console.log("Hola, mi nombre es: " + nombre + " " + apellido);
