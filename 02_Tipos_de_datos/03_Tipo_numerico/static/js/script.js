a0c5877a066d8781597519f8f13fb0f79a75ea69
nayaraperez <nayaraperez@liceovvh.cl>
2026-04-22 15:36:08 -0400

    ok

--- 02_Tipos_de_datos/03_Tipo_numerico/static/js/script.js
+++ 02_Tipos_de_datos/03_Tipo_numerico/static/js/script.js
@@ -1,43 +1,61 @@
-//Segunda tarea 
-console.log("Conexión exitosa js...");
-//tipos de datos numéricos
-//1.Declaraciom de numeros(creación de variables numéricas)
+console.log("Conexion con JS correcta...")
+//Tipos de satos numericos
+//let-->Palabra clave para crear una variable
+//1.- declaracion de numeros(creacion variacion numerica)
 let edad = 31;
 let temperatura = -5;
 let precio = 19990;
+let pi = 3.1416;
 let descuento = 0.25;
-//operaciones básicas 
+
+//2.- operaciones basicas
 let suma = 10 + 5; //15
-let resta = 10 - 5 ;//5
-let Multiplicacion = 10 * 5 ;//50
-let división = 10 / 2; //5
-let modulo = 10 % 3;//1
-let potencia = 2 ** 3;//8
+let resta = 10 - 5; //5
+let multiplicacion = 10 * 5; //50
+let division = 10 / 2; //5
+let modulo = 10 % 3; //1 resto de la divicion %
+let potencia = 2 ** 3; //8
 
-console.log (`El resultado de 2 elevado a 3 en potencia es: $(2**3).`)
-//3.-Incremento y decremento (suma(++) o resta(--)1)
+console.log(`El resultado de 2 elevado a 3 en potencia es: ${2**3}`)
+
+//3.- incremento y decremento (suma++) o resta (--) 1
 let numero = 10;
-numero++;//11
-numero--;//9
+numero++; //11-->incrementa 1 a la variante ++
+numero--;//9-->Decrementa 1 a la variables  --
 
-//4.-Numeros con decimales 
+//4.- Numeros con decimales
 let promedio = 6.5;
 let altura = 1.67;
-let peso =90.3;
-//5.-
-let poblacion = 1e6; //1.000.000
-console.log("Numero grande 1e6:" + poblacion)
-let numeroPequeno = 5e-3" + numeropequeno);
-//6.-operaciones combinadas 
-//Calculo de valor de hora trabajando 
+let peso = 90.3;
+
+//5.- Numeros grandes y notacion cientifica
+let poblacion = 1e6; //1000000
+console.log("Numero grande 1e6: " + poblacion);
+let NumeroPequeño = 5e-3; //0.005
+console.log("Numero pequeño 5e-3: " + NumeroPequeño);
+
+//6.- Operaciones combinadas
+//Calculo precios
 let precioProducto = 10000;
 let iva = 0.19;
-let total = precioProducto + (precioProducto * iva);//11900
-//calculo de valor de hora trabajada
+let total = precioProducto + (precioProducto * iva); //11900
+console.log("El precio dinal del producto es: " + total);
+//calculo de valor hora trabajada
 let horas = 40;
-let valorHora= 15000;
-let sueldo ="horas* valorHora";
-comsole.log("El sueldo base es:" + sueldo);
-//7.- Redondeo de numero 
-console.log ("Redondiado 4.6:" + Math.round(4.6)); //5
-console.log("Redondiado 4.6:"+ Math.floor(4.6));//4
+let valorHora = 12000;
+let sueldo = horas * valorHora;
+console.log("El sueldo base es:" +sueldo);
+//7.- Redondeo de numeros
+console.log("Redondeando 4.6: "+ Math.round(4,6)); //5
+console.log("Redondeando 4.6: "+ Math.floor(4.6));
+//numeros aliatorios
+let aliatorio = Math.random(); //entre 0 y 1
+let dado = Math.floor(Math.random ()*6) +1;
+console.log(`tirar dados: ${dado}`);
+//9.-Conparaciones numericas
+let a =10;
+let b = 5;
+console.log(a > b); //true
+console.log(a < b) //farse
+console.log (a === 10);// true( es estricta igualdad)
+console.log (b === 3);//Farse
